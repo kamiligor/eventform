5cf0162fefa05a55c453b604df637e0f211a6f1c
Kamil Kucharski <kamilkucharski@Kamils-MacBook-Pro.local>
2020-01-24 12:35:43 +0100

    Add form validation

--- src/EventForm/EventForm.js
+++ src/EventForm/EventForm.js
@@ -5,10 +5,34 @@ import DateFnsUtils from '@date-io/date-fns';
 
 class EventForm extends Component {
   state = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    eventDate: new Date(),
+    firstName: { value: ''},
+    lastName: { value: ''},
+    email: {value: ''},
+    eventDate: {
+      value: new Date(),
+    }
+  }
+
+  config = {
+    firstName: {
+      label: 'First name',
+      validationRules: {
+        required: true,
+      }
+    },
+    lastName: {
+      label: 'Last name',
+      validationRules: {
+        required: true,
+      }
+    },
+    email: {
+      label: 'Email',
+      validationRules: {
+        required: true,
+        email: true,
+      }
+    }
   }
 
   handleEventDate = date => {
@@ -17,50 +41,105 @@ class EventForm extends Component {
     })
   }
 
-  handleInput = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    })
+  handleBlur = e => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.validateField(name, value);
   }
 
+  handleInput = e => {
+    const name = e.target.name;
+    const value = e.target.value;
+  
+    // implicitly clear all other state properties (showError, msg, error)
+    const field = { value }
+    this.setState({[name]: field})
+  }
+  
+  handleSubmit = e => {
+    
+    if( this.validateAllFields() ) { 
+      //submit form
+      return true;
+    }
+    e.preventDefault();
+    
+  }
+  
+  validateField = (fieldName, fieldValue = '') => {
+    const validationRules = this.config[fieldName].validationRules;
+
+    const err  = {
+      error: false,
+      msg: '',
+    }
+
+    if ( validationRules.email && !this.emailIsValid(fieldValue)) {
+      err.error = true;
+      err.msg = 'Email is incorrect.';
+    }
+
+    if( validationRules.required && fieldValue === '' ) {
+      err.error = true;
+      err.msg = 'This field is required.';
+    }
+
+    this.setState(prevState => {
+      const field = {...prevState[fieldName]};
+      field.error = err.error;
+      field.msg = err.msg;
+        
+      return { [fieldName]: field }
+    });
+
+    return !err.error;
+  }
+
+  validateAllFields = () => {
+    let pass = true
+    for( let fieldName in this.config) {
+      if( !this.validateField(fieldName, this.state[fieldName].value) ) {
+        pass = false
+      }
+    }
+    return pass;
+  }
+
+  emailIsValid = email => /\S+@\S+\.\S+/.test(email);
+
   render() {
+    const textFields = [];
+    for( let fieldName in this.config) {
+      textFields.push(
+        <TextField
+          key={fieldName}
+          name={fieldName}
+          value={this.state[fieldName].value}
+          onChange={this.handleInput}
+          onBlur={this.handleBlur}
+          error={this.state[fieldName].error}
+          helperText={this.state[fieldName].msg}
+          id={fieldName}
+          label={this.config[fieldName].label}
+          variant="outlined"
+        />
+      )
+    }
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div className="form__main">
-          <TextField
-            name="firstName"
-            value={this.state.firstName}
-            onChange={this.handleInput}
-            id="firstName"
-            label="First name"
-            variant="outlined"
-          />
-          <TextField
-            name="lastName"
-            value={this.state.lastName}
-            onChange={this.handleInput}
-            id="lastName"
-            label="Last name"
-            variant="outlined"
-          />
-          <TextField
-            name="email"
-            value={this.state.email}
-            onChange={this.handleInput}
-            id="email"
-            label="Email"
-            variant="outlined"
-          />
+          {textFields}
           <MuiPickersUtilsProvider utils={DateFnsUtils}>
             <KeyboardDatePicker
               disableToolbar
+              disablePast
               autoOk
               name="eventDate"
               inputVariant="outlined"
               format="dd/MM/yyyy"
               id="date-picker-inline"
               label="Event date"
-              value={this.state.eventDate}
+              value={this.state.eventDate.value}
               onChange={this.handleEventDate}
             />
           </MuiPickersUtilsProvider>
